d0d4066b7c3d69183b1573c8529d254556af4e7b
Saahil Patel <Saahil@Saahils-MacBook-Pro.local>
2018-05-23 10:09:16 -0700

    Class MainWindow to display overall score

---
+++ grade_calculator.cpp
@@ -0,0 +1,27 @@
+#include "grade_calculator.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QObject::connect(ui->spinBox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall(int)));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::update_overall(int unused){
+    // double score = 31.4;
+    double score = static_cast<double>(unused);
+
+    ui -> horizontalSlider -> setValue(unused);
+    ui->label_14->setText(QString::number(score));
+
+    return;
+}
--- grade_calculator.h
+++ grade_calculator.h
@@ -15,6 +15,12 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+signals:
+    void compute_overall();
+
+public slots:
+    void update_overall(int);
+
 private:
     Ui::MainWindow *ui;
 };
